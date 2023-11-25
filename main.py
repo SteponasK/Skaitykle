1c818266cdd4e0be7e6c3ad6d7caf7349ef98e60
SteponasK <132608893+SteponasK@users.noreply.github.com>
2023-11-25 21:23:06 +0200

    Code refactoring

--- main.py
+++ main.py
@@ -2,21 +2,26 @@ from google.cloud import texttospeech
 import os
 import PyPDF2
 
+
 def get_text_string_from_pdf(pdf_path, start_page=0, end_page=None):
-    with open(pdf_path, 'rb' ) as file:
+    with open(pdf_path, "rb") as file:
         pdf_reader = PyPDF2.PdfReader(file)
-        text = ''
+        text = ""
         end_page = end_page or len(pdf_reader.pages)
         for page_num in range(start_page, end_page):
-         page = pdf_reader.pages[page_num]
-         text += page.extract_text()
+            page = pdf_reader.pages[page_num]
+            text += page.extract_text()
     return text
 
 
 def get_pdf_path():
-    pdf_path = input(r"Iveskite failo lokacija (Pvz.: C:\Users\Vardenis\Atsisiuntimai\Knyga.pdf):  ")
-    pdf_path = r"{}".format(pdf_path.replace('"', '').strip())
+    pdf_path = input(
+        r"Iveskite failo lokacija (Pvz.: C:\Users\Vardenis\Atsisiuntimai\Knyga.pdf):  "
+    )
+    pdf_path = r"{}".format(pdf_path.replace('"', "").strip())
     return pdf_path
+
+
 def main():
     if "GOOGLE_APPLICATION_CREDENTIALS" not in os.environ:
         print("GOOGLE_APPLICATION_CREDENTIALS nera nustatytas.")
@@ -30,8 +35,8 @@ def main():
 
     voice = texttospeech.VoiceSelectionParams(
         language_code="lt-LT",
-        name = "lt-LT-Standard-A",
-        ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL
+        name="lt-LT-Standard-A",
+        ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL,
     )
 
     audio_config = texttospeech.AudioConfig(
@@ -39,15 +44,14 @@ def main():
     )
 
     response = client.synthesize_speech(
-        input=synthesis_input,
-        voice=voice,
-        audio_config=audio_config
+        input=synthesis_input, voice=voice, audio_config=audio_config
     )
     output_file = "output.mp3"
     with open(output_file, "wb") as out:
         out.write(response.audio_content)
 
     print(f"Garsas yra irasytas i {output_file}")
+
 
 if __name__ == "__main__":
     main()
